9df6597fd4bb02642d069daf5d7031e0676279c8
osdnk <micosa97@gmail.com>
2018-10-03 20:55:54 +0200

    Initial commit

--- MobilizationPanRotateAndZoom/ViewController.swift
+++ MobilizationPanRotateAndZoom/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
+  @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+    let translation = recognizer.translation(in: self.view)
+    if let view = recognizer.view {
+      view.center = CGPoint(x:view.center.x + translation.x,
+                            y:view.center.y + translation.y)
+    }
+    recognizer.setTranslation(CGPoint.zero, in: self.view)
+  }
+  
+  @IBAction func handlePinch(recognizer:UIPinchGestureRecognizer) {
+    if let view = recognizer.view {
+      view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+      recognizer.scale = 1
+    }
+  }
+  
+  @IBAction func handleRotate(recognizer:UIRotationGestureRecognizer ) {
+    if let view = recognizer.view {
+      view.transform = view.transform.rotated(by: recognizer.rotation)
+      recognizer.rotation = 0
+    }
+  }
+  
+  func gestureRecognizer(_: UIGestureRecognizer,
+                         shouldRecognizeSimultaneouslyWith shouldRecognizeSimultaneouslyWithGestureRecognizer:UIGestureRecognizer) -> Bool {
+    return true
+  }
 
 }
 
