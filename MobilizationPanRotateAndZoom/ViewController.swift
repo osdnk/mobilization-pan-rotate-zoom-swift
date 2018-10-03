4bf3add22f6deabd5f2e33e4e0989fe5f1a8a46d
osdnk <micosa97@gmail.com>
2018-10-03 18:14:05 +0200

    Initial Commit

---
+++ MobilizationPanRotateAndZoom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobilizationPanRotateAndZoom
+//
+//  Created by Michał Osadnik on 03/10/2018.
+//  Copyright © 2018 Michał Osadnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
